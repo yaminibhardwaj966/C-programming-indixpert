3c5ad3fe423fc54ef80333a832b43b20e89d6016
kunalshahi25 <kunalshahi672@gmail.com>
2025-07-31 13:04:35 +0530

    this is just updated.

--- kunal.c
+++ kunal.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main(){
-    printf("This is my code. Kunal");
+    printf("This is my code. Kunal\n");
+    for (int i = 0; i < 4; i++)
+    {
+        printf("HELLO");
+        printf("\n");
+    }
+    
+    
+    
     return 0;
 }
