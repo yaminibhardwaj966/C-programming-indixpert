2fe68d003aef95b426a89a7f0167605a7afb9f65
sumanrathore-88 <sumanrathore.88@gmail.com>
2025-07-31 14:17:49 +0530

    some change

---
+++ number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int lessThan5 = 0;
+    int greaterThan5 = 0;
+
+    printf("Numbers from 1 to 10:\n");
+    for (i = 1; i <= 10; i++) {
+        printf("%d ", i);
+
+    
+        if (i < 5) {
+            lessThan5++;
+        } else if (i > 5) {
+            greaterThan5++;
+        }
+    }
+
+    
+    printf("\n\nCount of numbers less than 5: %d\n", lessThan5);
+    printf("Count of numbers greater than 5: %d\n", greaterThan5);
+
+    return 0;
+}
